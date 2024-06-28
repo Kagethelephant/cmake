e8b3d015f32e7d649394bb76b4e2a2526bc8aaad
Kage <kagethelephant@gmail.com>
2024-06-28 13:09:36 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "test.hpp"
+
+
+int main()
+{
+
+    int var = add(2,3);
+    std::cout << var << " is the number" << std::endl;
+
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+
+    if(window.isOpen())
+    {
+        std::cout << "window is something?" << std::endl;
+    }
+    else
+    {
+        std::cout << "window is nothing?" << std::endl;
+    }
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
